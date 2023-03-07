a8bfdac7d42bdb3898b28febb2cb22ce9f6d06a1
Mateus-Gabardo <gabardomateus@gmail.com>
2023-03-07 19:12:43 -0300

    #1 - B001 - Instanciar o projeto SpringBoot

---
+++ src/main/java/com/api/dronesetaecommerce/DronesetaEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.api.dronesetaecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DronesetaEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DronesetaEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/dronesetaecommerce/DronesetaEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.dronesetaecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DronesetaEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
