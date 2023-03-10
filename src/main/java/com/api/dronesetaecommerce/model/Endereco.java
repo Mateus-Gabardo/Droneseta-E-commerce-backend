3a2b883f12031df45f325b6d1d36c3970cead5b8
Daniel Larion Klug <dklug22@gmail.com>
2023-03-09 22:03:01 -0300

    #3 B003 - Desenvolver o crud de Endereço + DTO de endereço
    Serviço básico funcional

---
+++ src/main/java/com/api/dronesetaecommerce/model/Endereco.java
@@ -0,0 +1,131 @@
+package com.api.dronesetaecommerce.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "endereco")
+public class Endereco implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, length = 255)
+	private String logradouro;
+	
+	@Column(nullable = false)
+	private Integer numero;
+	
+	@Column(length = 100)
+	private String complemento;
+	
+	@Column(nullable = false, length = 100)
+	private String bairro;
+	
+	@Column(nullable = false, length = 100)
+	private String cidade;
+	
+	@Column(nullable = false, length = 100)
+	private String estado;
+	
+	@Column(nullable = false, length = 8)
+	private Integer cep;
+
+	public Endereco() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public Integer getCep() {
+		return cep;
+	}
+
+	public void setCep(Integer cep) {
+		this.cep = cep;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(bairro, cep, cidade, complemento, estado, id, logradouro, numero);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Endereco other = (Endereco) obj;
+		return Objects.equals(bairro, other.bairro) && Objects.equals(cep, other.cep)
+				&& Objects.equals(cidade, other.cidade) && Objects.equals(complemento, other.complemento)
+				&& Objects.equals(estado, other.estado) && Objects.equals(id, other.id)
+				&& Objects.equals(logradouro, other.logradouro) && Objects.equals(numero, other.numero);
+	}
+
+}
