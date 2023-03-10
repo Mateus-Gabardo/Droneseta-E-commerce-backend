a19bd49c7440495437b038ef6f062edc48c447e0
Daniel Larion Klug <dklug22@gmail.com>
2023-03-09 21:57:50 -0300

    Revert "#3"
    This reverts commit d9867eac49341a9c0f83fd314833ac7d844ee6a5.

--- src/main/java/com/api/dronesetaecommerce/repositories/EnderecoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.api.dronesetaecommerce.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.api.dronesetaecommerce.model.Endereco;
-
-public interface EnderecoRepository extends JpaRepository<Endereco, Long>{}
