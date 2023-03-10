d9867eac49341a9c0f83fd314833ac7d844ee6a5
Daniel Larion Klug <dklug22@gmail.com>
2023-03-09 21:56:19 -0300

    #3
    Serviço básico funcionando

---
+++ src/main/java/com/api/dronesetaecommerce/repositories/EnderecoRepository.java
@@ -0,0 +1,7 @@
+package com.api.dronesetaecommerce.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.api.dronesetaecommerce.model.Endereco;
+
+public interface EnderecoRepository extends JpaRepository<Endereco, Long>{}
---
+++ src/main/java/com/api/dronesetaecommerce/services/EnderecoServices.java
@@ -0,0 +1,73 @@
+package com.api.dronesetaecommerce.services;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.api.dronesetaecommerce.exceptions.ResourceNotFoundException;
+import com.api.dronesetaecommerce.model.Endereco;
+import com.api.dronesetaecommerce.repositories.EnderecoRepository;
+
+@Service
+public class EnderecoServices {
+	
+	private Logger logger = Logger.getLogger(EnderecoServices.class.getName());
+	
+	@Autowired
+	EnderecoRepository repository;
+	
+	public List<Endereco> findAll() {
+		logger.info("Procurando todos os endereços!");
+		
+		return repository.findAll();
+	}
+
+	public Endereco findById(Long id) {
+		logger.info("Procurando um endereço!");
+			
+		return repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+	}
+	
+	public Endereco create(Endereco endereco) {
+		logger.info("Salvando um novo endereço!");
+		
+		return repository.save(endereco);
+	}
+	
+	public Endereco update(Endereco endereco) {
+		logger.info("Atualizando um endereço!");
+		Endereco entity = repository.findById(endereco.getId())
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+		
+		if(endereco.getLogradouro() != null)
+			entity.setLogradouro(endereco.getLogradouro());
+		if(endereco.getNumero() != null)
+			entity.setNumero(endereco.getNumero());
+		if(endereco.getComplemento() != null)
+			entity.setComplemento(endereco.getComplemento());
+		if(endereco.getBairro() != null)
+			entity.setBairro(endereco.getBairro());
+		if(endereco.getCidade() != null)
+			entity.setCidade(endereco.getCidade());
+		if(endereco.getEstado() != null)
+			entity.setEstado(endereco.getEstado());
+		if(endereco.getCep() != null)
+			entity.setCep(endereco.getCep());
+		
+		return repository.save(entity);
+	}
+	
+	public void delete(Long id) {
+		logger.info("Deletando um endereço!");
+
+		Endereco entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Endereço com esse ID não foi encontrado!"));
+		repository.delete(entity);
+	}
+	
+	
+
+}
